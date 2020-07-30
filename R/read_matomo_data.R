e697af2065bb4f2df8076a35c0e5332c8d4734b0
Lars Schöbitz <lars.schoebitz@statistik.ji.zh.ch>
2020-07-30 14:58:37 +0200

    Remove query printing query

--- R/read_matomo_data.R
+++ R/read_matomo_data.R
@@ -104,18 +104,15 @@ read_matomo_data <- function(
 
   if (format == "csv") {
 
-    print(paste0("Query: ", query))
     utils::read.csv(query, encoding = "UTF-8", skipNul = TRUE, check.names = FALSE)
 
   } else if (format == "json") {
 
-    print(paste0("Query: ", query))
     jsonlite::fromJSON(txt = query)
 
 
   } else if (format == "xml") {
 
-    print(paste0("Query: ", query))
     xml2::read_xml(query)
 
   } else {
